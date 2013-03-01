1205a08966d6efba5e431908f4b74042e7e0848b
Timothy Kierzkowski <Timothydjk@gatech.edu>
2013-03-01 16:42:18 -0500

    added files

---
+++ WheresMyStuff/src/com/example/wheresmystuff/MenuActivity.java
@@ -0,0 +1,50 @@
+package com.example.wheresmystuff;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+
+public class MenuActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_menu);
+		
+		findViewById(R.id.Found_button).setOnClickListener(
+				new View.OnClickListener() {
+					@Override
+					public void onClick(View view) {
+						//Put your code here
+						finish();
+					}
+				});
+		
+		findViewById(R.id.lost_button).setOnClickListener(
+				new View.OnClickListener() {
+					@Override
+					public void onClick(View view) {
+						//Put your code here
+						finish();
+					}
+				});
+		
+		findViewById(R.id.Donate_button).setOnClickListener(
+				new View.OnClickListener() {
+					@Override
+					public void onClick(View view) {
+						//Put your code here
+						finish();
+					}
+				});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_menu, menu);
+		return true;
+	}
+
+}
