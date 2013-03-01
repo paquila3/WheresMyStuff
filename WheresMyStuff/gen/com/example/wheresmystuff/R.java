ddf8755820fba4ca1184c7d3a35b3a4e4b7a41ce
Timothy Kierzkowski <Timothydjk@gatech.edu>
2013-03-01 16:45:35 -0500

    Hopefully it now works.

--- WheresMyStuff/gen/com/example/wheresmystuff/R.java
+++ WheresMyStuff/gen/com/example/wheresmystuff/R.java
@@ -14,37 +14,47 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int Donate_button=0x7f070007;
+        public static final int Found_button=0x7f070008;
         public static final int email=0x7f070003;
         public static final int login=0x7f070005;
         public static final int login_form=0x7f070002;
         public static final int login_status=0x7f070000;
         public static final int login_status_message=0x7f070001;
-        public static final int menu_forgot_password=0x7f070007;
+        public static final int lost_button=0x7f070009;
+        public static final int menu_forgot_password=0x7f07000a;
+        public static final int menu_settings=0x7f07000b;
         public static final int password=0x7f070004;
         public static final int sign_in_button=0x7f070006;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
+        public static final int activity_menu=0x7f030001;
     }
     public static final class menu {
         public static final int activity_login=0x7f060000;
+        public static final int activity_menu=0x7f060001;
     }
     public static final class string {
-        public static final int action_sign_in_register=0x7f040005;
-        public static final int action_sign_in_short=0x7f040006;
+        public static final int Donate=0x7f040010;
+        public static final int Found=0x7f04000e;
+        public static final int Lost=0x7f04000f;
+        public static final int action_sign_in_register=0x7f040006;
+        public static final int action_sign_in_short=0x7f040007;
         public static final int app_name=0x7f040000;
-        public static final int error_field_required=0x7f04000c;
-        public static final int error_incorrect_password=0x7f04000b;
+        public static final int error_field_required=0x7f04000d;
+        public static final int error_incorrect_password=0x7f04000c;
         public static final int error_incorrect_password_user_locked=0x7f040002;
-        public static final int error_invalid_email=0x7f040009;
-        public static final int error_invalid_password=0x7f04000a;
+        public static final int error_invalid_email=0x7f04000a;
+        public static final int error_invalid_password=0x7f04000b;
         public static final int error_user_locked=0x7f040001;
-        public static final int login_progress_signing_in=0x7f040008;
-        public static final int menu_forgot_password=0x7f040007;
+        public static final int login_progress_signing_in=0x7f040009;
+        public static final int menu_forgot_password=0x7f040008;
+        public static final int menu_settings=0x7f040003;
         /**  Strings related to login 
          */
-        public static final int prompt_email=0x7f040003;
-        public static final int prompt_password=0x7f040004;
+        public static final int prompt_email=0x7f040004;
+        public static final int prompt_password=0x7f040005;
     }
     public static final class style {
         /** 
