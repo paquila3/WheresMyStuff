6e5ddc92c46aeafa45b517038374374785c663bd
Timothy Kierzkowski <Timothydjk@gatech.edu>
2013-03-01 22:05:52 -0500

    Cory's Files, hopefully correct

--- WheresMyStuff/gen/com/example/wheresmystuff/R.java
+++ WheresMyStuff/gen/com/example/wheresmystuff/R.java
@@ -36,25 +36,23 @@ public final class R {
         public static final int activity_menu=0x7f060001;
     }
     public static final class string {
-        public static final int Donate=0x7f040011;
-        public static final int Found=0x7f04000f;
-        public static final int Lost=0x7f040010;
-        public static final int action_sign_in_register=0x7f040007;
-        public static final int action_sign_in_short=0x7f040008;
+        public static final int Donate=0x7f04000f;
+        public static final int Found=0x7f04000d;
+        public static final int Lost=0x7f04000e;
+        public static final int action_sign_in_register=0x7f040005;
+        public static final int action_sign_in_short=0x7f040006;
         public static final int app_name=0x7f040000;
-        public static final int error_field_required=0x7f04000e;
-        public static final int error_incorrect_password=0x7f04000d;
-        public static final int error_incorrect_password_user_locked=0x7f040004;
-        public static final int error_invalid_email=0x7f04000b;
-        public static final int error_invalid_password=0x7f04000c;
-        public static final int error_user_locked=0x7f040003;
-        public static final int login_progress_signing_in=0x7f04000a;
-        public static final int menu_forgot_password=0x7f040009;
+        public static final int error_field_required=0x7f04000c;
+        public static final int error_incorrect_password=0x7f04000b;
+        public static final int error_invalid_email=0x7f040009;
+        public static final int error_invalid_password=0x7f04000a;
+        public static final int login_progress_signing_in=0x7f040008;
+        public static final int menu_forgot_password=0x7f040007;
         public static final int menu_settings=0x7f040001;
         /**  Strings related to login 
          */
-        public static final int prompt_email=0x7f040005;
-        public static final int prompt_password=0x7f040006;
+        public static final int prompt_email=0x7f040003;
+        public static final int prompt_password=0x7f040004;
         public static final int title_activity_menu=0x7f040002;
     }
     public static final class style {
