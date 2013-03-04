26c40ad0034a465bb4186eb3bfe85669e941a163
Olivia Davis <odavis3@gatech.edu>
2013-03-03 20:35:38 -0500

    AddItemActivity - adding something you lost

--- WheresMyStuff/gen/com/example/wheresmystuff/R.java
+++ WheresMyStuff/gen/com/example/wheresmystuff/R.java
@@ -24,48 +24,68 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int Donate_button=0x7f080007;
-        public static final int Found_button=0x7f080008;
-        public static final int email=0x7f080003;
-        public static final int login=0x7f080005;
-        public static final int login_form=0x7f080002;
-        public static final int login_status=0x7f080000;
-        public static final int login_status_message=0x7f080001;
-        public static final int lost_button=0x7f080009;
-        public static final int menu_forgot_password=0x7f08000a;
-        public static final int menu_settings=0x7f08000b;
-        public static final int password=0x7f080004;
-        public static final int sign_in_button=0x7f080006;
+        public static final int Donate_button=0x7f080016;
+        public static final int Found_button=0x7f080017;
+        public static final int action_settings=0x7f08001b;
+        public static final int button1=0x7f080003;
+        public static final int button2=0x7f08000e;
+        public static final int digitalClock1=0x7f080006;
+        public static final int editText1=0x7f080005;
+        public static final int editText2=0x7f080000;
+        public static final int editText3=0x7f080009;
+        public static final int editText4=0x7f08000b;
+        public static final int editText5=0x7f08000d;
+        public static final int email=0x7f080012;
+        public static final int linearLayout1=0x7f080002;
+        public static final int login=0x7f080014;
+        public static final int login_form=0x7f080011;
+        public static final int login_status=0x7f08000f;
+        public static final int login_status_message=0x7f080010;
+        public static final int lost_button=0x7f080018;
+        public static final int menu_forgot_password=0x7f080019;
+        public static final int menu_settings=0x7f08001a;
+        public static final int password=0x7f080013;
+        public static final int sign_in_button=0x7f080015;
+        public static final int textView1=0x7f080004;
+        public static final int textView2=0x7f080001;
+        public static final int textView3=0x7f080007;
+        public static final int textView4=0x7f080008;
+        public static final int textView5=0x7f08000a;
+        public static final int textView6=0x7f08000c;
     }
     public static final class layout {
-        public static final int activity_login=0x7f030000;
-        public static final int activity_menu=0x7f030001;
+        public static final int activity_add_item=0x7f030000;
+        public static final int activity_login=0x7f030001;
+        public static final int activity_menu=0x7f030002;
     }
     public static final class menu {
         public static final int activity_login=0x7f070000;
         public static final int activity_menu=0x7f070001;
+        public static final int add_item=0x7f070002;
     }
     public static final class string {
-        public static final int Donate=0x7f050013;
-        public static final int Found=0x7f050011;
-        public static final int Lost=0x7f050012;
+        public static final int Donate=0x7f050015;
+        public static final int Found=0x7f050013;
+        public static final int Lost=0x7f050014;
         public static final int action_settings=0x7f050006;
-        public static final int action_sign_in_register=0x7f050009;
-        public static final int action_sign_in_short=0x7f05000a;
+        public static final int action_sign_in_register=0x7f05000b;
+        public static final int action_sign_in_short=0x7f05000c;
         public static final int app_name=0x7f050000;
-        public static final int error_field_required=0x7f050010;
-        public static final int error_incorrect_password=0x7f05000f;
+        public static final int error_field_required=0x7f050012;
+        public static final int error_incorrect_password=0x7f050011;
         public static final int error_incorrect_password_user_locked=0x7f050002;
-        public static final int error_invalid_email=0x7f05000d;
-        public static final int error_invalid_password=0x7f05000e;
+        public static final int error_invalid_email=0x7f05000f;
+        public static final int error_invalid_password=0x7f050010;
         public static final int error_user_locked=0x7f050001;
-        public static final int login_progress_signing_in=0x7f05000c;
-        public static final int menu_forgot_password=0x7f05000b;
+        public static final int hello_world=0x7f050008;
+        public static final int login_progress_signing_in=0x7f05000e;
+        public static final int menu_forgot_password=0x7f05000d;
         public static final int menu_settings=0x7f050003;
         /**  Strings related to login 
          */
-        public static final int prompt_email=0x7f050007;
-        public static final int prompt_password=0x7f050008;
+        public static final int prompt_email=0x7f050009;
+        public static final int prompt_password=0x7f05000a;
+        public static final int title_activity_add_item=0x7f050007;
         public static final int title_activity_main=0x7f050005;
         public static final int title_activity_menu=0x7f050004;
     }
