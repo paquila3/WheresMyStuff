01b799dc59d84326c6c43bb1b9781a1401b56e21
Cory Walker <fatboy95ci@yahoo.com>
2013-03-04 12:47:17 -0500

    Added logout

--- WheresMyStuff/gen/com/example/wheresmystuff/R.java
+++ WheresMyStuff/gen/com/example/wheresmystuff/R.java
@@ -24,16 +24,17 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int Donate_button=0x7f080007;
-        public static final int Found_button=0x7f080008;
+        public static final int Donate_button=0x7f080009;
+        public static final int Found_button=0x7f08000a;
         public static final int email=0x7f080003;
         public static final int login=0x7f080005;
         public static final int login_form=0x7f080002;
         public static final int login_status=0x7f080000;
         public static final int login_status_message=0x7f080001;
-        public static final int lost_button=0x7f080009;
-        public static final int menu_forgot_password=0x7f08000a;
-        public static final int menu_settings=0x7f08000b;
+        public static final int logout_button=0x7f080007;
+        public static final int lost_button=0x7f080008;
+        public static final int menu_forgot_password=0x7f08000b;
+        public static final int menu_settings=0x7f08000c;
         public static final int password=0x7f080004;
         public static final int sign_in_button=0x7f080006;
     }
@@ -48,6 +49,7 @@ public final class R {
     public static final class string {
         public static final int Donate=0x7f050013;
         public static final int Found=0x7f050011;
+        public static final int Logout=0x7f050014;
         public static final int Lost=0x7f050012;
         public static final int action_settings=0x7f050006;
         public static final int action_sign_in_register=0x7f050009;
