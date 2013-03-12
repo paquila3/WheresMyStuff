c1ef4403556baf49eb29e5bd135b76692a58faad
Cory Walker <fatboy95ci@yahoo.com>
2013-03-11 20:38:51 -0400

    Fixed crashing admin class

--- WheresMyStuff/src/com/example/wheresmystuff/AdminActivity.java
+++ WheresMyStuff/src/com/example/wheresmystuff/AdminActivity.java
@@ -11,7 +11,7 @@ public class AdminActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_menu);
+		setContentView(R.layout.activity_admin);
 		
 		findViewById(R.id.Add_Admin).setOnClickListener(
 				new View.OnClickListener() {
