a94e333d31d24e4f50d4a1e272edbbde55a609c4
Timothy Kierzkowski <Timothydjk@gatech.edu>
2013-04-03 11:56:25 -0400

    Forgot to use the MVP format. my bad

--- WheresMyStuff/src/com/example/wheresmystuff/view/SearchMenuActivity.java
+++ WheresMyStuff/src/com/example/wheresmystuff/view/SearchMenuActivity.java
@@ -1,21 +1,25 @@
-package com.example.wheresmystuff;
+package com.example.wheresmystuff.view;
+
+import com.example.wheresmystuff.R;
+import com.example.wheresmystuff.R.layout;
+import com.example.wheresmystuff.R.menu;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class UserProfile extends Activity {
+public class SearchMenuActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_user_profile);
+		setContentView(R.layout.activity_search_menu);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.user_profile, menu);
+		getMenuInflater().inflate(R.menu.search_menu, menu);
 		return true;
 	}
 
--- WheresMyStuff/src/com/example/wheresmystuff/view/UserProfileActivity.java
+++ WheresMyStuff/src/com/example/wheresmystuff/view/UserProfileActivity.java
@@ -8,18 +8,18 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class SearchMenu extends Activity {
+public class UserProfileActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_search_menu);
+		setContentView(R.layout.activity_user_profile);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.search_menu, menu);
+		getMenuInflater().inflate(R.menu.user_profile, menu);
 		return true;
 	}
 
