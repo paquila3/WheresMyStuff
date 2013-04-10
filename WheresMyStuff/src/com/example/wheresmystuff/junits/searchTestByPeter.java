36b881abacab2c3396a0d95b57955ad9e9cda047
Peter <Peter@Peter-THINK>
2013-04-10 19:01:44 -0400

    Made my actual JUnit

--- WheresMyStuff/src/com/example/wheresmystuff/junits/searchTestByPeter.java
+++ WheresMyStuff/src/com/example/wheresmystuff/junits/searchTestByPeter.java
@@ -2,16 +2,41 @@ package com.example.wheresmystuff.junits;
 
 import android.test.AndroidTestCase;
 import junit.framework.Assert;
+import com.example.wheresmystuff.presenter.WheresMyStuff;
+import com.example.wheresmystuff.model.*;
 
 public class searchTestByPeter extends AndroidTestCase {
-	private int testInt;
+	private Item lostItem1, lostItem2, foundItem1, foundItem2;
 	
 	@Override
 	public void setUp() {
-		testInt = 2;
+		lostItem1 = new LostItem("LostItem1", "Lost Item", 0, "", 0, "", "", "", 0, 0);
+		lostItem2 = new LostItem("LostItem2", "Lost Item", 0, "", 0, "", "", "", 0, 0);
+		foundItem1 = new LostItem("FoundItem1", "Found Item", 0, "", 0, "", "", "", 0, 0);
+		foundItem2 = new LostItem("FoundItem2", "Found Item", 0, "", 0, "", "", "", 0, 0);
 	}
 	
 	public void testCase1() throws Throwable {
-		Assert.assertTrue(testInt > 0);
+		Assert.assertTrue(WheresMyStuff.search(lostItem1, "lostitem1", ""));
+	}
+	
+	public void testCase2() throws Throwable {
+		Assert.assertTrue(WheresMyStuff.search(lostItem2, "lostitem2", "") && WheresMyStuff.search(lostItem2, "lostitem2", "lost item"));
+	}
+	
+	public void testCase3() throws Throwable {
+		Assert.assertTrue(WheresMyStuff.search(lostItem1, "", "lost item") && WheresMyStuff.search(lostItem2, "", "lost item"));
+	}
+	
+	public void testCase4() throws Throwable {
+		Assert.assertTrue(WheresMyStuff.search(foundItem1, "founditem1", ""));
+	}
+	
+	public void testCase5() throws Throwable {
+		Assert.assertTrue(WheresMyStuff.search(foundItem2, "founditem2", "") && WheresMyStuff.search(foundItem2, "founditem2", "found item"));
+	}
+	
+	public void testCase6() throws Throwable {
+		Assert.assertTrue(WheresMyStuff.search(foundItem1, "", "found item") && WheresMyStuff.search(foundItem2, "", "found item"));
 	}
 }
